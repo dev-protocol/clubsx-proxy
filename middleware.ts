60bc7dee9393c8b40510328d046ca6889a90edbb
aggre <hiroyuki.aggre@gmail.com>
2023-11-17 15:18:46 +0900

    Update middleware.ts
    Add `/starter` for redirection dist

--- middleware.ts
+++ middleware.ts
@@ -16,7 +16,7 @@ const redirects = [
 		host,
 		matchers: [
 			"/",
-			new RegExp("^/(ticketing|plugins|dev-tokens|blog|post|pricing)(|/.*)$"),
+			new RegExp("^/(starter|ticketing|plugins|dev-tokens|blog|post|pricing)(|/.*)$"),
 		],
 		destination: "https://www.clubs.place",
 	})),
